e544fe37c3727b7c302b73faa8c0096ff1d128f4
Mian Jawad Ahmad <a.jawad321@gmail.com>
2020-10-04 20:48:39 +0500

    Update env.js

--- env.js
+++ env.js
@@ -1,13 +1,13 @@
-export const dialogflowConfig = {
-    "type": "service_account",
-    "project_id": "healthbot-aitv",
-    "private_key_id": "5982112bb6b6907726265e226bbdd937c73ff93c",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQDFB0Fc7EhziIJL\nUiIyIuE0Y0xY+DqvnOKOWICr7uyLmP+Gzv2zl7696RzTqHmcTm0pWcJ+tu+WlwdN\nNWtEMpcugfZr8b+uvyrV4x2JFrhuOWBmOTQCC6ec11lYt7SYhzlA6tqgADz07499\nb2quot++lXwwDg6avZxGNNyFf/6x4ds+grxKuU1+dra5H1lXc19HS+9kAjwU1Pni\nE2iy/ITLaXhkJQN3KX3VHEF9yxHZK+bKhLo6N1F2yZ2r3fpGQXy943lyxl/N/+sJ\nkM1abfJ+XZ223hfuvNgUQQlsAvUrbRChWSu5akxK16TOHLI9D5jS3p4DvGzH+7r0\n53Pl/Ku/AgMBAAECggEAAcZ3dc1hN71Go0QZr8b1rYGpifGuoa9xsSOmASaVdgqK\nXt4Vlh0raE2l51hQxDJGtIUtBfRDA2HX/H+bOFGWYCJmpIFYn0v0qFm0Beyw+7lD\nn5grWktdlsGv+ecp1uf9AZM8Jg3qfrB74/7xgKJSDscNB+fQhN9pfY5zYDaPhm3E\n+Ww9erGopq2N04OWm+twJBy3fXKLqs41h1gHYgEDfXOgCAKCzM3Ta07UZmVpbINg\n7FPy6ANp+NE9SgbXZm/jaWTF7b0RvI/CRFAyb6nVTCDqGIJxzKWQ8aR1c+2wsKzd\nTZTcrhdfl6J+WhlZa8ZkCdSwtM0rf5T7ycQjCLJ/5QKBgQD3n1cJwT2UPHTGB+oj\n6vRCSTrAwDispB/Ld67mxs4z54+yEl94SxZXePNXwgUeKF2eX874F3DtqSiOLX0h\nkg8dArEOUVx8vnkhennElW0KPJP70HXacDgeWfhTFN3oG54Wu7rCN49Re2/2beCM\naGjrGbZuU4VSYh9Qwrdq9dmb4wKBgQDLsbg0MPuutQVoGbnQ11drORbGFnDagXPK\nPzs5SrxMyVr5auXslIBNiVscys/IzbiiSPM32xqAUOW6n3yfymYHmisgH+Wruo5y\n0k2NDpZGkQguLiObv2O844L7GprrhX0MWG7TvhfPq5n5/KcBJLgZywykGFD8KIYy\n0AaAqhtvdQKBgC824pGpluQ84scfmzVeB1TcQaee/y9r2+Qf3emC3q5m6K87cZiu\nOIRVO5DwHamyaKdZ/Uywq4elSWZSphlQkDEn4jqoqMngR5hqke050KCzDERnaxFL\ndGfVH9Q3llg7a9lJ0kJV9a3Dq4WBTd93yQmKzY4bvsAGlZIAjpiGaAehAn88eIY2\n+FO/if4Q6ZJAba2GJ2ybYg4e0E+PuzXZzFYadayMXy/jDDMK0bw6z/rfojMt+no6\nGtHDRkDtJmvZBD1GBH7JCq8kTD392HEtbycty+Ch1RC9LBRv/xrjcQKM6RQ2lgpQ\n0h4/iLIthNQPgtck0SLvlEh2k5FDhH5ptBZxAoGAVNYGekAAKvsFkULlUlw0FE/k\nbSE6Hvrx0vhhsNAg3NTOcfNeTE2KbBnLSbP5ldqk/0Lyueq7vLpGSptPfiym7G2U\nVjNHsNsPrjtQlYy/l6SM393Exjcf3PByTbVEHYc5bzztJUqUPRNriUDTbsnVcSO+\nFlUPxE1AyyS5hulyXQI=\n-----END PRIVATE KEY-----\n",
-    "client_email": "dialogflow-efdjcb@healthbot-aitv.iam.gserviceaccount.com",
-    "client_id": "116332727177957320544",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-efdjcb%40healthbot-aitv.iam.gserviceaccount.com"
-  }
-  
+/* export const dialogflowConfig = {
+  "type": "service_account",
+  "project_id": "Health-bot",
+  "private_key_id": "xxxx",
+  "private_key": "-----BEGIN PRIVATE KEY-----\n xxxx\n-----END PRIVATE KEY-----\n",
+  "client_email": "xxxx",
+  "client_id": "xxxx",
+  "auth_uri": "xxxx",
+  "token_uri": "xxxx",
+  "auth_provider_x509_cert_url": "xxxx",
+  "client_x509_cert_url": "xxxx"
+}
+*/
